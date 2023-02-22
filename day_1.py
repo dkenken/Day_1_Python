b48650f459adb38c818ac44c57dd9dc4b51ff0c1
dkenken <dkenken72@gmail.com>
2023-02-21 22:05:19 -0800

    First commit

---
+++ day_1.py
@@ -0,0 +1,111 @@
+# Class Exercises Day #1
+
+# Objective: Writing simple Python Print Function.
+
+# Day 1 - Python Print Function
+print('Day 1 - Python Print Function')
+
+# The function is declared like this:
+print('The function is declared like this:')
+
+# print('what to print')
+print("print('what to print')")
+
+
+
+# Come up with my own imaginative exercises and solve them.
+
+# I consent that I won't give up and always follow your dreams.
+print("I consent that I won't give up. If I fall, I will always get up, and fight back.")
+
+# Always show love but It does not mean allowing peole to treat you wrong.
+print('Always show love but It does not mean allowing people to treat you wrong.')
+
+# Nothing is in life is fair, life was never meant to be fair. Work hard , play hard!
+print('Nothing is in life is fair, life was never meant to be fair. Work hard , play hard!\n')
+
+print('----------------------------------------------------------------------------------------------------------------------------------------------------------------------\n')
+
+# Day #1 Exercise #2
+
+# Objective: String Manipulation 
+
+#Fix the code below:
+
+# print('Day 1 - String Manipulation")
+print("Day 1 - String Manipulation")
+
+# print(String Concatenation is done with the '+" sign."")
+print('String Concatenation is done with the "+" sign.')
+
+# print('e.g. print("Hello " + "world"))
+print('e.g. print("Hello " + "world")')
+
+# print('New lines can be created with a backslash and n.)
+print("New lines can be created with a backslash and n.\n")
+
+print('-----------------------------------------------------------------------------------------------------------------------------------------------------------------------\n')
+
+# Day #1 Exercise #3
+
+# Objective: Write a program that prints the number of characters in a user's name.
+
+# Write your code below:
+
+name = input('What is your name?\n')
+print(len(name))
+print(' ')
+
+print('-------------------------------------------------------------------------------------------------------------------------------------------------------------\n')
+
+# Day 1 Exercise #4
+
+# Objective: Write a program that switches the values stored in the variables a and b.
+
+# Write your code below:
+
+a = input("a: ")
+b = input("b: ")
+
+
+####################################
+# Write your code below this line 
+
+c = a
+a = b
+b = c
+
+#Write your code above this line
+####################################
+
+# Don't change the code below
+print("a: " + a)
+print("b: " + b)
+
+print('-------------------------------------------------------------------------------------------------------------------------------------------------------------------\n')
+
+# Day 1 Challenge #1
+
+#1. Create a greeting for your program.
+print('Welcome to the Greeting Program.')
+#2. Ask the user for the city that they grew up in.
+city = input('Where did you grow up?\n')
+#3. Ask the user for the name of a pet.
+pet = input("What is the name of your pet?\n")
+#4. Combine the name of their city and pet and show them their band name.
+band_name = city + " " + pet
+print(f'Your band name is {band_name}')
+#5. Make sure the input cursor shows on a new line:
+
+print('--------------------------------------------------------------------------------------------------------------------------------------------------------------------\n')
+
+# Day 1 Challenge #2
+
+#1 Create a Welcome program message for each cleint at the ZOO.
+print('Welcome to the most Amazing Zoo in Washington State!')
+#2 Ask the client how far did you travel to come here.
+travel = input("How far did you travel to get here?\n")
+#3 ask the client What is your fave animal.
+fave_animal = input('What is your fave animal during this visit?\n')
+#4 Thank client for visiting the ZOO.
+print('Thanks for coming and see ya next time!' )
